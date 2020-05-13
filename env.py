fa8be39c2915347d0d5e0dd8a781ea4ed3506a4e
brosnans <s.brosnan3@gmail.com>
2020-05-13 20:34:04 +0000

    Created Heroku App

--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 import os
 
-os.environ.setdefault("SECRET_KEY", '2342545243524^&^$%£%56675$&$&$%&$%6') 
+os.environ.setdefault("SECRET_KEY", 'g@eg7l3%mnxbz&kog#yoe&ik7+)c-l7iy2x7e_)*h_%-ay^c%^') 
 
+os.environ.setdefault("DATABASE_URL", "postgres://vrgeizcviydamd:a024da804991e743b25673bb1fe06a20df2c355849fe1ee7ccabfb4624e6642a@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/dca58dgd223rbp")
